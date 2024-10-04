99eae218f6ea556ba4c640fbffd0fdda5eaea41a
Eduardo B. <146993235+kkewink@users.noreply.github.com>
2024-10-04 14:47:35 -0300

    ..

---
+++ pet_adopt/lib/view/dashboard_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Center(
+                child: Text("Sing Up"),
+              )
+            ],
+          ),
+        ),
+      ),
+
+    );
+  }
+}
